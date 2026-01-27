21a9dc501d46b010ec1d0efbd3330f0e1a9170e0
MrOrangeUS <71477247+MrOrangeUS@users.noreply.github.com>
2026-01-27 11:51:41 -0800

    Add web layout.tsx

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import { ToastProvider } from '../components/ToastProvider';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ToastProvider>{children}</ToastProvider>
+      </body>
+    </html>
+  );
+}
